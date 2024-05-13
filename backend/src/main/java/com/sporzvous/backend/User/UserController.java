929fa9ec1f317a8fd0db994ef676eeca250328bb
emreerol0 <emreerol.erol0@gmail.com>
2024-05-14 00:34:44 +0300

    add create feedback

--- backend/src/main/java/com/sporzvous/backend/User/UserController.java
+++ backend/src/main/java/com/sporzvous/backend/User/UserController.java
@@ -2,13 +2,12 @@ package com.sporzvous.backend.User;
 
 import com.sporzvous.backend.Feedback.Feedback;
 import com.sporzvous.backend.Feedback.FeedbackService;
+import com.sporzvous.backend.MailSender.MailSenderService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
