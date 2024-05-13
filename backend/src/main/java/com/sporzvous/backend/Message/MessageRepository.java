84e6f48f7dafc2a103f8441ec01877bfedcd6c2f
berksengul17 <berksengul17@gmail.com>
2024-05-13 22:51:02 +0300

    implement message entity

---
+++ backend/src/main/java/com/sporzvous/backend/Message/MessageRepository.java
@@ -0,0 +1,14 @@
+package com.sporzvous.backend.Message;
+
+import com.sporzvous.backend.User.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface MessageRepository  extends JpaRepository<Message, Long> {
+    Optional<List<Message>> findByReceiver(User receiver);
+    Optional<List<Message>> findBySenderAndReceiver(User sender, User receiver);
+}
