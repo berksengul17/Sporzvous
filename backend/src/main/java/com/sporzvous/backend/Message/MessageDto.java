901f1cf678ff7c9a366d04288f23c3b1a0c87724
berksengul17 <berksengul17@gmail.com>
2024-05-30 03:22:44 +0300

    implement chat backend

--- backend/src/main/java/com/sporzvous/backend/Message/MessageDto.java
+++ backend/src/main/java/com/sporzvous/backend/Message/MessageDto.java
@@ -1,2 +1,17 @@
-package com.sporzvous.backend.Message;public class MessageDto {
+package com.sporzvous.backend.Message;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class MessageDto {
+    private Long receiverId;
+    private Long senderId;
+    private String content;
+    private LocalDateTime timeStamp;
 }
