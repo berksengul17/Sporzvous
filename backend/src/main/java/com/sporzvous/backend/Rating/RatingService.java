202f93f86fdce896630ddd3dff22324447ac80c7
emreerol0 <emreerol.erol0@gmail.com>
2024-06-02 12:55:52 +0300

    overall hesaplamaları ve yerleştirmeleri

--- backend/src/main/java/com/sporzvous/backend/Rating/RatingService.java
+++ backend/src/main/java/com/sporzvous/backend/Rating/RatingService.java
@@ -1,12 +1,14 @@
 package com.sporzvous.backend.Rating;
 
+import com.sporzvous.backend.SportRating.SportRating;
 import com.sporzvous.backend.User.User;
 import com.sporzvous.backend.User.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
@@ -27,5 +29,44 @@ public class RatingService {
                 .orElseThrow(() -> new IllegalArgumentException("User not found"));
         Rating rating = new Rating(userRating, sportField, content, user);
         return ratingRepository.save(rating);
+    }
+
+    public Map<SportField, Double> getRatingByOthers(Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("User could not found by getting rating by others"));
+
+        // Group ratings by sport and calculate the average for each sport
+
+        return user.getRatings().stream()
+                .collect(Collectors.groupingBy(Rating::getSportField,
+                        Collectors.averagingDouble(Rating::getRating)));
+    }
+
+
+    public Map<SportField, Double> getOverallRating(Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("User could not be found for getting rating by others"));
+
+        // Fetch other users' ratings
+        Map<SportField, Double> otherUsersAverageRatings = user.getRatings().stream()
+                .collect(Collectors.groupingBy(Rating::getSportField,
+                        Collectors.averagingDouble(Rating::getRating)));
+
+        // Fetch self-ratings
+        Map<SportField, Double> selfRatings = user.getSportRatings().stream()
+                .collect(Collectors.groupingBy(SportRating::getSportField,
+                        Collectors.averagingDouble(SportRating::getRating)));
+
+        // Combine ratings
+        return otherUsersAverageRatings.entrySet().stream()
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        entry -> {
+                            SportField sportField = entry.getKey();
+                            double otherUsersAverage = entry.getValue();
+                            double selfRating = selfRatings.getOrDefault(sportField, 0.0);
+                            return 0.6 * otherUsersAverage + 0.4 * selfRating;
+                        }
+                ));
     }
 }
