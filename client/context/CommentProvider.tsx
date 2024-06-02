3bbb58512adc1953a9b9fa6c8d5a69aeccab4064
emreerol0 <emreerol.erol0@gmail.com>
2024-06-03 00:35:53 +0300

    comments bozulacak

--- client/context/CommentProvider.tsx
+++ client/context/CommentProvider.tsx
@@ -1,9 +1,20 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
+import axios from "axios";
+import { User } from "./UserProvider";
 
-type Comment = {};
+type Comment = {
+  id: number;
+  type: string;
+  rating: number;
+  commentor: User;
+  commentDate: string;
+  commentPreview: string;
+  profilePicUrl: string;
+};
 
 type CommentProps = {
   comments: Comment[];
+  fetchComments: () => void;
 };
 
 const API_URL = process.env.EXPO_PUBLIC_API_URL + "/api/ratings";
@@ -17,7 +28,32 @@ export const CommentProvider = ({
 }) => {
   const [comments, setComments] = useState<Comment[]>([]);
 
-  const value = { comments };
+  const fetchComments = async () => {
+    try {
+      const response = await axios.get(`${API_URL}/get-all-comments`);
+      console.log("RESPONSE", response.data);
+
+      const comments = response.data.map((comment: Comment) => {
+        console.log("Processing event:", comment);
+        return {
+          ...comment,
+          commentDate: new Date(comment.commentDate).toLocaleDateString(),
+        };
+      });
+
+      console.log("MY Comments", comments);
+
+      return comments;
+    } catch (error) {
+      console.error("Failed to fetch comments", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchComments();
+  }, []);
+
+  const value = { comments, fetchComments };
 
   return (
     <CommentContext.Provider value={value}>{children}</CommentContext.Provider>
@@ -27,7 +63,7 @@ export const CommentProvider = ({
 export const useCommentContext = () => {
   const context = useContext(CommentContext);
   if (!context) {
-    throw new Error("useCommentContext must be used within a UserProvider");
+    throw new Error("useCommentContext must be used within a CommentProvider");
   }
   return context;
 };
